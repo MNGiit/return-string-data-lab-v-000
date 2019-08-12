86590b00f7de11376b373077a2a1aaf772836a67
Mohammed Noori <kmanoorib@gmail.com>
2019-08-12 03:31:29 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,31 @@
 class ProductsController < ApplicationController
-
+  def index
+    @products = Product.all
+  end
+  
+  def inventory
+    item = Product.find(params[:id])
+    
+  end
+  
+  def desciption 
+    
+  end
+  
+  def new
+    @product = Product.new
+  end
+  
+  def create
+    Product.create(product_params)
+    
+    redirect_to products_path
+  end
+  
+  private
+  
+  def product_params
+    param.require(:product).permit(:name, :description, :inventory, :price)
+  end
+  
 end
